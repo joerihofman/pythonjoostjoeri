7fce3a276dca2ee704707b5af6dece9d1cafd17d
Joost Holwerda <joostholwerda@hotmail.com>
2016-09-12 15:48:10 +0200

    assignment 2

---
+++ Opgave2.py
@@ -0,0 +1,21 @@
+#opdracht 2
+
+listL = [30, 1, 2, 1, 0, "hello", "Goodbye"];
+
+# a
+# showing the position in the list. It searches the value in the list.
+listL.index(1)
+
+
+# b
+# counting the total amount of the value entered.
+listL.count(1)
+
+# c
+# total amount of items in the list
+len(listL)
+
+# d
+# max gives max INT value. This has to be an int else
+max(listL)
+print( max(listL))
