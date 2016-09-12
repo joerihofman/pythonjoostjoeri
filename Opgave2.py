4674c17feb781c6c099589fcbf9cacc5f20259ec
qwerty12794 <onzin09@gmail.com>
2016-09-12 16:05:23 +0200

    opgave2

--- Opgave2.py
+++ Opgave2.py
@@ -1,21 +1,31 @@
+import random
 #opdracht 2
 
 listL = [30, 1, 2, 1, 0, "hello", "Goodbye"];
-
+listM = [30, 1, 2, 1, 0];
 # a
 # showing the position in the list. It searches the value in the list.
-listL.index(1)
+#listL.index(1)
 
 
 # b
 # counting the total amount of the value entered.
-listL.count(1)
+#listL.count(1)
 
 # c
 # total amount of items in the list
-len(listL)
+#len(listL)
 
 # d
 # max gives max INT value. This has to be an int else
-max(listL)
-print( max(listL))
+#max(listL)
+
+
+
+#l
+# sorts the list from small to large
+#(listM.sort()
+
+#m
+# randomises a list
+#random.shuffle(listL)
