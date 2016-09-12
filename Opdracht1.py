4da946fe5c6e8dfb1b4b669ceec2680341e3b27e
qwerty12794 <onzin09@gmail.com>
2016-09-12 15:13:16 +0200

    al opdracht1&2

---
+++ Opdracht1.py
@@ -0,0 +1,12 @@
+#opdracht 1a:
+list1 = ['2', '3', '4'];
+print(list1);
+
+#opdracht 1b:
+list2 = ['red', 'green', 'blue'];
+print(list2);
+
+#opdracht 1c:
+a= range(3,5,1);
+list3 = a;
+print(list3);
