65dbf8cae777d007208c397e390aa4c9d5e8ee17
qwerty12794 <onzin09@gmail.com>
2016-09-14 10:26:49 +0200

    opgave2&3 klaar

--- opgave4.py
+++ opgave4.py
@@ -2,3 +2,11 @@ L1 = [30, 1, 2, 1, 0];
 L2 = [1, 21, 13];
 
 #a
+#L3 = L1+L2
+#print(L3)
+#result: [30, 1, 2, 1, 0, 1, 21, 13], the two lists are combined.
+
+#b
+#l4= 3*L2
+#print(l4)
+#result: [1, 21, 13, 1, 21, 13, 1, 21, 13] the list is repeated 3 times.
