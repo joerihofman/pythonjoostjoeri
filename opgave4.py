543766e5b8f5690619a2b1fd32146a44dd1e943a
qwerty12794 <onzin09@gmail.com>
2016-09-14 10:44:08 +0200

    opgave2&3 klaar

--- opgave4.py
+++ opgave4.py
@@ -10,3 +10,15 @@ L2 = [1, 21, 13];
 #l4= 3*L2
 #print(l4)
 #result: [1, 21, 13, 1, 21, 13, 1, 21, 13] the list is repeated 3 times.
+
+#c
+#L1 > L2
+#Result: True; the first value of both lists are compared.
+
+
+
+
+
+#e
+#[x for x in L1 if x in L1]
+#result: [30, 1, 2, 1, 0]; the statement reads the list, puts in the value of each x on place x, if x is in L1
