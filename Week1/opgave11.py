26b9efeb7e4ca96427be658590f499592a05dce6
qwerty12794 <onzin09@gmail.com>
2016-09-20 11:39:43 +0200

    opgave 11 nog bezig

--- Week1/opgave11.py
+++ Week1/opgave11.py
@@ -3,6 +3,13 @@ regels = []
 
 for regel in gegevens:
     if regel[0].isdigit():
+        split = regel.split()
+        #dag = int(split[0])
+        #uur = int(split[1])
+        temp = int(split[2])
         regels.append(regel)
+    #while len(regels)<dag:
+    #    regels.append(regel)
+    #    regels[dag-1].append(temp)
 
 print(regels)
