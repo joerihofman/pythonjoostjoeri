cef85ee3dfba1339964f4f51a78aff471ce216e5
qwerty12794 <onzin09@gmail.com>
2016-09-29 11:44:17 +0200

    opdrachten week 2

--- Week2/opdracht11.py
+++ Week2/opdracht11.py
@@ -3,3 +3,23 @@ students = { 0: "Nynke", 1: "Lolle", 2: "Jikke", 3: "Popke", 4: "Teake",
 
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4), (4, 5),
                (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+#lijstVrienden = []
+#for index in students:
+#    lijstVrienden.append(index)
+#print(lijstVrienden)
+
+length = len(students)
+i = 0
+friendlist = []
+
+while i < length:
+    friendlist.append([])
+    friends = 0
+    for n in friendships:
+        if n[0] == i:
+            friendlist[i].append(n[1])
+        elif n[1] == i:
+            friendlist[i].append(n[0])
+    i += 1
+print(friendlist)
