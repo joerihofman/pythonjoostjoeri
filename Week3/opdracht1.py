e56e1c1caa40b25581b4c6ef9a990f4ae1983f25
qwerty12794 <onzin09@gmail.com>
2016-09-29 13:59:17 +0200

    opdrachten week 2

--- Week3/opdracht1.py
+++ Week3/opdracht1.py
@@ -1,3 +1,5 @@
+"""
+A:
 class A:
     def __init__(self, i = 0):
         self.i = i
@@ -17,3 +19,36 @@ def main():
     print(b.i, b.j)
 
 main()
+"""
+#output: 3 0
+#        4 0
+
+class A:
+    def __init__(self, i):
+        self.i = i
+    def __str__(self):
+        return "I am class A"
+
+class B(A):
+    def __init__(self, i, j):
+        super().__init__(i)
+        self.j = j
+    def __str__(self):
+        return "I am class B"
+
+def main():
+    a = A(1)
+    b = B(1, 2)
+    print(a)
+    print(b)
+    print(a.__dict__)
+    print(b.__dict__)
+main()
+
+"""
+OUTPUT:
+I am class A
+I am class B
+{'i': 1}
+{'j': 2, 'i': 1}
+"""
