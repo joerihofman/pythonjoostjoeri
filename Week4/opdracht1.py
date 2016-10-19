57390d16492fe6d7aab0826799879f23d34d3241
qwerty12794 <onzin09@gmail.com>
2016-10-19 16:07:05 +0200

    opdrachten week 4

--- Week4/opdracht1.py
+++ Week4/opdracht1.py
@@ -29,10 +29,22 @@ y2 = value_to_y(randint(0,100))
 
 class Plot:
     def __init__(self, val):
-        self.canvas = Tk()
-        self.canvas.title('Simple Plot')
-        self.canvas = Canvas(Tk(), width=1200, height=600, bg='white')
+        self.root = Tk()
+        self.root.title('Simple Plot')
+        self.canvas = Canvas(Tk(), width=1200, height=600, bg='white')#0,0 =topleftcorner
         self.canvas.pack(expand=YES, fill=BOTH)
+        Button(self, text='Quit', command=self.root.quit).pack()
+        self.canvas.create_line(50,550,1150,550, width=2)
+        self.canvas.create_line(50,550,50,50, width=2)
+        for i in range(23): #x-axis
+            x = 50+(i*50)
+            self.canvas.create_line(x, 550, x, 50, width=1, dash=(2, 5))
+            self.canvas.create_text(x, 550, text='%d' % (10 * i), anchor=N)
+        for i in range(11):
+            y = 550 - (i * 50)
+            self.canvas.create_line(50, y, 1150, y, width=1, dash=(2, 5))
+            self.canvas.create_text(40, y, text='%d' % (10 * i), anchor=E)
+
 
 
     def step(self):
@@ -49,4 +61,5 @@ class Plot:
         self.canvas.create_line(x1, y1, x2, y2, fill='blue', tags='temp')
         # print(s, x1, y1, x2, y2)
         s = s + 1
-        self.canvas.after(300, step)
+        self.canvas.after(300, step())
+        self.root.mainloop()
